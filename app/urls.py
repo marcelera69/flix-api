824813722f35f95f6fcb0047ac9028f1a8f01fff
Marcelo Soares <ceeloalves@hotmail.com>
2024-04-29 19:48:03 -0300

    create at Retrive update destroy view

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListVIew, genre_detail_view
+from genres.views import GenreCreateListVIew, GenreRetriveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('genres/', GenreCreateListVIew.as_view() , name='genre-create-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view')
+    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view() , name='genre-detail-view')
 ]
