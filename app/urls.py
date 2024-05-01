c83399646fb6d40857a86d1c5a5598cbac8a5a5f
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-01 13:50:57 -0300

    Crud completo de movies

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroy
 from genres.views import GenreCreateListVIew, GenreRetriveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
+     path('movies/<int:pk>', MovieRetrieveUpdateDestroy.as_view(), name='movie-detail-view'),
 
     path('genres/', GenreCreateListVIew.as_view() , name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view() , 
