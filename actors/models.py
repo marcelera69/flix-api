4da4caa92dfffb3fbe6f873b4ce186d8f83a60a9
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-01 12:55:21 -0300

    criando model da app actors e criação de choices para nationality

--- actors/models.py
+++ actors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+NATIONALITY_CHOICES = (
+    ('USA', 'Estados Unidos'),
+    ('BRAZIL', 'Brasil'),
+    ('FRANCE', 'França'),
+    ('GERMANY', 'Alemnaha'),
+    ('UK', 'Inglaterra'),
+    ('IT', 'Italia'),
+    ('ES', 'Espanha'),
+)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField( max_length=50, choices=NATIONALITY_CHOICES, blank=True, null=True )
+    
+    def __str__(self):
+        return self.name
--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'genres',
-    'actors,'
+    'actors',
 ]
 
 MIDDLEWARE = [
