dfca685e91ccd50540aeb47c99133d90220a2c02
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-03 11:59:52 -0300

    versionando a url do app actors

---
+++ actors/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+import views
+
+urlpatterns = [
+    path('actors/', views.ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>', views.ActorRetrieveUpdateDestroyView.as_view(), 
+         name='actor-detail-view' ),
+]
--- app/urls.py
+++ app/urls.py
@@ -8,15 +8,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
      path('api/v1/', include('genres.urls')),
+     path('api/v1/', include('actors.urls')),
      
 
 
      path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
      path('movies/<int:pk>', MovieRetrieveUpdateDestroy.as_view(), name='movie-detail-view'),
 
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), 
-         name='actor-detail-view' ),
 
      path('reviews', ReviewCreateListView.as_view(), name='review-create-list'),
      path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
