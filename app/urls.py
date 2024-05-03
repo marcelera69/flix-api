e7414a7b07a1a8f505d4046973a6aecf69bfed36
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-03 12:03:24 -0300

    versionando as urls da app reviews

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,9 +7,5 @@ urlpatterns = [
      path('api/v1/', include('genres.urls')),
      path('api/v1/', include('actors.urls')),
      path('api/v1/', include('movies.urls')),
-     
-
-
-     path('reviews', ReviewCreateListView.as_view(), name='review-create-list'),
-     path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+     path('api/v1/', include('reviews.urls')),
 ]
