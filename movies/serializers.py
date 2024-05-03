1cd0ecf4878304fe47396ea13d511ab32c5233ad
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-03 12:53:53 -0300

    criando validaçoes no serializer da app movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 300:
+            raise serializers.ValidationError('Resumo deve ter menos que 300 caracteres')
+        return value
