e539ab05eaf7ceff08c0730c263b0066f57b2d78
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-20 11:50:16 -0300

    adicionando a app authentication no installed apps

--- app/settings.py
+++ app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework_simplejwt',
     
+    'authentication',
     'movies',
     'genres',
     'actors',
