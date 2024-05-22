0fdf691f6d0fb8d22a62ef98dba21101fbc3513b
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-22 10:03:56 -0300

    Protegendo os endpoints da api com token

--- genres/views.py
+++ genres/views.py
@@ -1,16 +1,19 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializers import GenreSerializer
 
 
 
 class GenreCreateListVIew(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
      path('movies/', views.MovieCreateListView.as_view(), name='movie-create-list'),
-     path('movies/<int:pk>', views.MovieRetrieveUpdateDestroy.as_view(), 
+     path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroy.as_view(), 
           name='movie-detail-view'),
 ]
