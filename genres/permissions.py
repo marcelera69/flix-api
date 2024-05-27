f2a2d5604916b2aca0cb7f4fea02dad03404cd09
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-27 10:50:42 -0300

    adicionando permissao para o patch e put. - Corrigindo erro na perm de post

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,10 +4,18 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermission):
     
     def has_permission(self, request, view):
-        if request.method == 'GET' or 'HEAD' or 'OPTIONS':
+        print(request.method)
+
+        if request.method in ['GET','HEAD','OPTIONS']:
+            print('Entrou GET')
             return request.user.has_perm('genres.view_genre')
 
         if request.method == 'POST':
-            return False
+            print('Entrou POST')
+            return request.user.has_perm('genres.add_genre')
+
+        if request.method =='PATCH' or 'PUT':
+            print('Entrou PATCH')
+            return request.user.has_perm('genres.change_genre')
 
         return False
