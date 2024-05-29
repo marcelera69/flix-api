aec648b2759c04107c0e3d7913e9d3ad2f6736b5
Marcelo Soares <ceeloalves@hotmail.com>
2024-05-29 10:07:12 -0300

    endpoint de estatísticas (parte 2)

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,10 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
 from app.permissions import GlobalPermissions
-
+from reviews.models import Review
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
@@ -26,7 +27,17 @@ class MovieStatsView(views.APIView):
         # Buscar todos os dados
         # Montar resposta
         # Devolve resposta pro user com estatíticas
+        total_movies = self.queryset.count()
+        movies_by_genere = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
         return response.Response(
-            data={'messege': 'Funcionou'},
+            data={
+                'total_movies': total_movies,
+                'movies_by_genere': movies_by_genere,
+                'totalt_reviews':total_reviews,
+                'average_stars': round(average_stars, 1) if average_stars else 0,
+            },
             status=status.HTTP_200_OK,
         )
