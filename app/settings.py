784d5625ba36e97973b9644af02db3a7e63e2d8a
Marcelo Soares <ceeloalves@hotmail.com>
2024-06-05 11:03:46 -0300

    configurando a aplicação para usar o BD postgresql

--- app/settings.py
+++ app/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flix',
+        'USER': 'postgres',
+        'PASSWORD': 'vicini123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
